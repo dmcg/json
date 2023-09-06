b3029156f0393b8a481c33ca40d4000cd5088fd4
Duncan McGregor <duncan@oneeyedmen.com>
2023-09-06 14:41:13 +0100

    Add examples and strikt

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.strikt:strikt-core:0.34.0")
 }
 
 tasks.test {
---
+++ src/test/kotlin/com/oneeyedmen/json/examples.kt
@@ -0,0 +1,59 @@
+package com.oneeyedmen.json
+
+val aNull = "null"
+
+val aBool = "true"
+val anotherBool = "false"
+val bools = listOf(aBool, anotherBool)
+
+val anInt = "42"
+val aFloat = "12.34"
+val anENumber = "12.34E5"
+val aNegative = "-12.34"
+val numbers = listOf(
+    aBool,
+    anInt,
+    aFloat,
+    anENumber,
+    aNegative,
+)
+
+val aString = "banana"
+val anEmptyString = ""
+val aBlankString = "  "
+val aStringWithASpace = "Hello world"
+val aStringWithAQuote = """Hello \"world\""""
+val strings = listOf(
+    aString,
+    anEmptyString,
+    aBlankString,
+    aStringWithASpace,
+    aStringWithAQuote,
+    // TODO("other escapes")
+)
+
+val anEmptyObject = "{}"
+val anotherEmptyObject = "{  }"
+val anEmptyObjectWithALineBreak = "{\n}"
+val emptyObjects = listOf(
+    anEmptyObject,
+    anotherEmptyObject,
+    anEmptyObjectWithALineBreak,
+)
+
+val aSingleLevelObject = """
+    {
+        "aNull":null,
+        "aBool" : true,
+        "aNumber" :-12.34,
+        "aString": "banana"
+    }
+    """.trimIndent()
+val aSingleLevelObjectWithNoWhitespace = aSingleLevelObject
+    .replace("""\s""".toRegex(), "")
+val singleLevelObjects = listOf(
+    aSingleLevelObject,
+    aSingleLevelObjectWithNoWhitespace,
+)
+
+// TODO arrays, multi-level objects
